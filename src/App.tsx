9e5a0be65a8749f50e3964f8d25c7a87510aa98a
Alice Moretti <ali.moret88@gmail.com>
2022-10-25 10:02:29 +0300

    Coded App.ts wih function structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import QuestionCard from "./Components/QuestionCard";
 
 function App() {
-  return <div className="App"></div>;
+  const startQuiz = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>Quiz</h1>
+      <button className="start" onClick={startQuiz}>
+        Start the quiz
+      </button>
+      <p className="score">your score:</p>
+      <p>Loading questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next question
+      </button>
+    </div>
+  );
 }
 
 export default App;
