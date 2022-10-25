99813fdd122c2cfcf1cbe6be53721cba25c3b240
Alice Moretti <ali.moret88@gmail.com>
2022-10-25 11:09:48 +0300

    Added Question type to specify type of all fetched properties

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,13 @@
 //logic to fetch data from https://opentdb.com/api.php?amount=10type=multiple
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answer: string[];
+  question: string;
+  type: string;
+};
+
 export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
