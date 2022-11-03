4644b0f8e681d2c24bd23c001e038b7af59cca80
Alice Moretti <ali.moret88@gmail.com>
2022-11-03 11:28:48 +0200

    Created GlobalStyle file using styled components

---
+++ src/App.style.ts
@@ -0,0 +1,20 @@
+import styled, { createGlobalStyle } from "styled-components";
+import background from "./Assets/background.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+box-sizing: border-box;
+margin: 0;
+}
+
+body {
+background-image: url(${background});
+background-size:cover;
+font-family: 'Source Sans Pro', sans-serif;
+display:flex;
+
+justify-content:center;
+height: 100vh
+}
+
+`;
